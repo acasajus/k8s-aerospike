87ed88f9c1b01dbf4d6dbd84ca73310aebbffa26
Adria Casajus <adriancasajus@gmail.com>
2017-06-08 12:42:04 +0200

    Discovery of neighbours

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,10 @@ FROM aerospike:3.12.1
 
 MAINTAINER Adrià Casajús <acasajus@infern.us>
 
-ADD kubectl/kubectl /kubectl
-
 ENV DOCKERIZE_VERSION v0.4.0
 
-RUN apt-get update && apt-get install -y curl jq \
-    && curl https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz -o dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+RUN apt-get update && apt-get install -y wget dnsutils \
+    && wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
     && tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
     && rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
     && rm -rf /var/lib/apt/lists/*
@@ -16,7 +14,6 @@ COPY run.sh /run.sh
 COPY aerospike.conf /etc/aerospike/aerospike.conf
 
 RUN chmod +x /run.sh
-RUN chmod +x /kubectl
 
 ENTRYPOINT []
 
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e
 
 #$1 - parameter; $2 - value; $3 - default value; $4 - file;
 Template() {
-    [[ -z "$2" ]] &&  sed -i "s/{{ $1 }}/$3/g" $4 || sed -i "s/{{ $1 }}/$2/g" $4
+	[[ -z "$2" ]] &&  sed -i "s/{{ $1 }}/$3/g" $4 || sed -i "s/{{ $1 }}/$2/g" $4
 }
 
 CONF="/etc/aerospike/aerospike.conf"
@@ -13,7 +13,26 @@ Template as_memory_size_gb "$AS_MEMORY_SIZE_GB" 1 $CONF
 Template as_default_ttl_days "$AS_DEFAULT_TTL_DAYS" 0 $CONF
 Template as_file_size_gb "$AS_FILE_SIZE_GB" 1 $CONF
 
-/kubectl proxy &
+AddPeers() {
+	dockerize -wait tcp://127.0.0.1:3002	
+	dockerize -wait tcp://127.0.0.1:3000	
 
-asd --foreground
+	iface=$(ip route | grep default | awk '{print $NF}')
+	myIP=$(ip addr show dev eth0 | grep 'inet ' | awk '{print $2}' | sed 's:/.*$::')
+
+	podIPs=$(dig ${AS_SERVICE:-"aerospike-svc.default"}.svc.cluster.local | grep 'IN A' | awk '{print $NF}')
+
+	echo "All pods IP are: $podIPs"
+
+	for podIP in $podIPs
+	do
+		if [ "$podIP" != "$myIP" ]; then
+			dockerize -wait tcp://$podIP:3002
+			asinfo -v "tip:host=$podIP;port=3002"
+			echo "Added $podIP as peer"
+		fi
+	done
+}
+
+AddPeers & asd --foreground
 
